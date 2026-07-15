223d91e3c12d1a057876de94acbdbe83b57f56f2
ramsrib <1449806+ramsrib@users.noreply.github.com>
2026-07-15 11:56:20 -0700

    clipwire: add formula (v0.1.0)

---
+++ Formula/clipwire.rb
@@ -0,0 +1,18 @@
+class Clipwire < Formula
+  desc "Paste clipboard images into a program on a remote host over SSH"
+  homepage "https://github.com/ramsrib/clipwire"
+  url "https://github.com/ramsrib/clipwire/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "7bf8b36376613d2b4d4272add737245df9667589377c7c26c09e539fa64942d1"
+  license "MIT"
+  head "https://github.com/ramsrib/clipwire.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    assert_match "clipwire #{version}", shell_output("#{bin}/clipwire --version")
+  end
+end
